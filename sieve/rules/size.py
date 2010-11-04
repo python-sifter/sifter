b5dc636611ea3009bd6d3c27f3e651604ecff0d9
Gary Peck <gary@realify.com>
2010-11-03 17:28:49 -0700

    fix case of tags

--- sieve/rules/size.py
+++ sieve/rules/size.py
@@ -9,14 +9,14 @@ class SieveTestSize(base.SieveTest):
         base.SieveTest.__init__(self, arguments, tests)
         self.validate_arguments_size(2)
         self.validate_tests_size(0)
-        self.validate_arg_is_tag(0, ('over', 'under'))
+        self.validate_arg_is_tag(0, ('OVER', 'UNDER'))
         self.validate_arg_is_number(1)
 
     def evaluate(self, message, state):
         # FIXME: size is defined as number of octets, whereas this gives us
         # number of characters
         message_size = len(message.as_string())
-        if self.arguments[0].tag == 'over':
+        if self.arguments[0].tag == 'OVER':
             return message_size > self.arguments[1]
-        elif self.arguments[1].tag == 'under':
+        elif self.arguments[0].tag == 'UNDER':
             return message_size < self.arguments[1]
