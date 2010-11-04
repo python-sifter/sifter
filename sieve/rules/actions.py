37ff7f50fd981aef039c7db91ed28584586ff18c
Gary Peck <gary@realify.com>
2010-11-04 01:48:49 -0700

    make SieveActions inherit from list. simple delegation doesn't work with new-style classes.

--- sieve/rules/actions.py
+++ sieve/rules/actions.py
@@ -1,17 +1,14 @@
 
-class SieveActions(object):
+class SieveActions(list):
 
     def __init__(self, implicit_keep=False):
-        self.actions = []
+        super(SieveActions, self).__init__()
         self.implicit_keep = implicit_keep
 
     def append(self, action, action_args=None):
-        self.actions.append((action, action_args))
+        super(SieveActions, self).append((action, action_args))
         return self
 
     def cancel_implicit_keep(self):
         self.implicit_keep = False
         return self
-
-    def __getattr__(self, name):
-        return getattr(self.actions, name)
