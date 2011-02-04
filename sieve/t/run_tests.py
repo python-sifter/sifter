dae971995d57b11a8a3174bd4e056dc23ab7ebd8
Gary Peck <gary@realify.com>
2011-02-04 14:10:55 -0500

    print a bit more status when running tests

--- sieve/t/run_tests.py
+++ sieve/t/run_tests.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
                 'sieve.t.test_validators',
             ),
         )
-    unittest.TextTestRunner().run(suite)
+    unittest.TextTestRunner(verbosity=2).run(suite)
 
