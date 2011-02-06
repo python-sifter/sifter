c8d6b22d932ff0ec53ea6d5a769927b1d403666f
Gary Peck <gary@realify.com>
2011-02-06 17:40:39 -0500

    don't include abstract comparison functions in sieve.grammar.Comparator
    this allows sieve.comparator.get_match_fn to properly detect when a
    specific comparison type is not supported by a comparator (by the lack
    of a defined function for that match type)

--- sieve/grammar/comparator.py
+++ sieve/grammar/comparator.py
@@ -18,14 +18,3 @@ class Comparator(object):
     def sort_key(cls, s):
         return s
 
-    @classmethod
-    def cmp_is(cls, str1, str2):
-        raise NotImplementedError
-
-    @classmethod
-    def cmp_contains(cls, s, substring):
-        raise NotImplementedError
-
-    @classmethod
-    def cmp_matches(cls, s, pattern):
-        raise NotImplementedError
