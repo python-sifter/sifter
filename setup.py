72c0694cdfd9b5eb1f4de159c9353bafefed85a6
Manfred Kaiser <manfred.kaiser@logfile.at>
2020-09-28 16:00:55 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-with open('README.rst') as file:
-    long_description = file.read()
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="sifter",
-    version="0.1",
-    author="Gary Peck",
-    author_email="gary@realify.com",
+    version="0.2.0",
+    author="Gary Peck, Manfred Kaiser",
+    author_email="gary@realify.com, manfred.kaiser@logfile.at",
     url="https://github.com/garyp/sifter",
     license="BSD",
-    description="Parser/evaluator for the Sieve filtering language (RFC 5228)",
     long_description=long_description,
+    long_description_content_type='text/x-rst',
+    project_urls={
+        'Source': 'https://github.com/garyp/sifter',
+        'Tracker': 'https://github.com/garyp/sifter/issues',
+    },
+    python_requires='>= 3.6',
     install_requires=[
         "ply",
     ],
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: BSD License",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
